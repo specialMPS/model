382a7adf7e9497b9dc5893d191f9d717dd8eb1a3
tngus2sh <ssuhyeon98@gmail.com>
2022-11-04 08:05:01 +0900

    kogpt2:modification

--- model/chat/kogpt2_chat.py
+++ model/chat/kogpt2_chat.py
@@ -7,15 +7,16 @@ from transformers import PreTrainedTokenizerFast, GPT2LMHeadModel
 
 from preprocess.train_gpt import KoGPT2Chat
 
-
+import kss
 from eunjeon import Mecab
+from hanspell import spell_checker
 
 mecab = Mecab()
 # from konlpy.tag import Kkma
 #
 # kkma = Kkma()
 
-parser = argparse.ArgumentParser(description='Simsimi based on KoGPT-2')
+parser = argparse.ArgumentParser(description='Simsini based on KoGPT-2')
 
 parser.add_argument('--chat',
                     action='store_true',
@@ -29,7 +30,7 @@ parser.add_argument('--sentiment',
 
 parser.add_argument('--model_params',
                     type=str,
-                    default='././checkpoint/model_-last_test.ckpt',
+                    default='././checkpoint/model_-last.ckpt',
                     help='model binary for starting chat')
 
 parser.add_argument('--train',
@@ -87,43 +88,77 @@ class KoGPT2Chat(LightningModule):
         return output.logits
 
     def chat(self, input_sentence, sent='0'):
-        # test mecab
-        # mecab으로 품사 태깅
-        # test kkma
-        # kkma로 품사 태깅
+        # 1. kss로 문장분류
+        s = input_sentence
+        bf_sent = ''  # 이전 문장
+        sent_len = len(kss.split_sentences(s))  # 쪼개진 문장 전체 길이
+        for i, sent in enumerate(kss.split_sentences(s), start=1):  # 쪼개진 문장 중에 마지막 문장만 가져오기
+            if sent_len != i:
+                bf_sent = sent
+            else:
+                mpos = mecab.pos(input_sentence)
+                for index, value in enumerate(mpos):
+                    if 'SY' in value[1] or 'SC' in value[1]:
+                        input_sentence = bf_sent
+                    elif 'MAG' == value[1]:
+                        input_sentence = bf_sent
+                    else:
+                        input_sentence = sent
+        print("kss : {}".format(input_sentence))
+
+        # 2. hanspell로 문장 맞춤법 검사
+        result = spell_checker.check(input_sentence)
+        result = result.as_dict()  # dict 형태로 변환
+        input_sentence = result['checked']
+        print("hanspell : {}".format(input_sentence))
+
+        # 3. mecab을 이용해 형태소 분석 후 되묻는 질문 만들기
         # total check
         jjx = False
 
-        # k_pos = kkma.pos(input_sentence)
         m_pos = mecab.pos(input_sentence)
         print(input_sentence)
+        print(m_pos)
 
         for index, value in enumerate(m_pos):
             # 처음 인사할 때 제외
-            if input_sentence == '안녕' or input_sentence == '안녕하세요':
+            if '안녕' in value[0] or '안녕하세요' in value[0] or '하이' in value[0]:
+                jjx = True
+                break
+            elif '고마워' in value[0] or '고마워요' in value[0]:
                 jjx = True
                 break
             # 목적어가 생략되어 있는지 확인, 목적어가 없다고 하더라도 다른 형태가 목적어를 대신하고 있는지 확인
+            # 목적격 조사
             elif 'JKO' in value[1]:
                 jjx = True
                 print('jko true')
                 break
+            # 주격 조사
             elif 'JKS' in value[1]:
                 jjx = True
                 print('jks true')
                 break
+            # 보격 조사
             elif 'JKC' in value[1]:
                 jjx = True
                 print('jkc true')
                 break
+            # 형용사 파생 접미사
             elif 'XSA' in value[1]:
                 jjx = True
                 print('xsa true')
                 break
+            # 마침표, 물음표, 느낌표
             elif 'SF' in value[1] and '?' in value[0]:
                 jjx = True
-            # elif '모르' in value[0]:
-            #     jjx = True
+                break
+            elif '모르' in value[0] or '몰라' in value[0]:
+                jjx = True
+            # 접속 부사
+            elif 'MAJ' in value[1]:
+                jjx = True
+                break
             else:
                 print('jjx false')
 
@@ -133,21 +168,21 @@ class KoGPT2Chat(LightningModule):
         if jjx == False:
             # 마지막에 마침표 물음표 느낌표가 있는지 확인
             # 있다면 index를 1을 줄여서 확인
-            if 'SF' in m_pos[ind][1]:
-                print('sf')
-                ind = ind - 1
-            if 'EC' in m_pos[ind][1] or 'EF' in m_pos[ind][1]:
-                ind = ind - 1
-            if 'EP' in m_pos[ind][1]:
-                print('ep')
-                if 'VV' in m_pos[ind - 1] or 'VA' in m_pos[ind - 1] or 'XR' in m_pos[ind - 1]:
-                    answer = "왜 " + m_pos[ind - 1][0] + m_pos[ind][0] + "어요??"
-                    return answer
-                else:
-                    answer = "왜 " + m_pos[ind][0] + "어요??"
-                    return answer
-            # answer = "왜요?"
-            # return answer
+            # if 'SF' in m_pos[ind][1]:
+            #     print('sf')
+            #     ind = ind - 1
+            # if 'EC' in m_pos[ind][1] or 'EF' in m_pos[ind][1]:
+            #     ind = ind - 1
+            # if 'EP' in m_pos[ind][1]:
+            #     print('ep')
+            #     if 'VV' in m_pos[ind - 1] or 'VA' in m_pos[ind - 1] or 'XR' in m_pos[ind - 1]:
+            #         answer = "왜 " + m_pos[ind - 1][0] + m_pos[ind][0] + "어요??"
+            #         return answer
+            #     else:
+            #         answer = "왜 " + m_pos[ind][0] + "어요??"
+            #         return answer
+            answer = "좀 더 자세히 말해줄 수 있나요?"
+            return answer
             jjx = True
 
         if jjx == True:
@@ -165,17 +200,7 @@ class KoGPT2Chat(LightningModule):
                     if gen == EOS or gen == PAD: # PAD 무한 루프 에러 방지
                         break
                     a += gen.replace('▁', ' ')
-
                 a = a.strip()
-                period_pos = a.rfind(".")
-                question_pos = a.rfind("?")
-                exclamation_pos = a.rfind("!")
-                last_pos = len(a) - 1
-                # 마지막 위치가 . ? !와 같은 위치이면 한 문장이 끝나는 것이여서 그대로 a 반환
-                if last_pos == period_pos or last_pos == question_pos or last_pos == exclamation_pos:
-                    return a
-                mark_pos = max(max(period_pos, question_pos), exclamation_pos)
-                a = a[:mark_pos + 1]
                 if a == "":
                     return "듣고 있어요. 계속 얘기해주세요!"
                 return a
